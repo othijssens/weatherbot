f69f3fedfad9fa34f21d3e5d5a9471cfce96202d
othijssens <olivier.thijssens@pacmed.nl>
2022-06-01 11:00:40 +0200

    remove precipitation

--- src/get-message.ts
+++ src/get-message.ts
@@ -3,28 +3,14 @@ import checkItsNiceOut from "./nice-out";
 import getAlerts from "./alerts";
 import { IncomingWebhookSendArguments } from "@slack/webhook";
 
-export default function getMessage({ hourly, currently, alerts }: Weather) {
+export default function getMessage({ currently, alerts }: Weather) {
   const listAlerts = getAlerts(alerts);
   const niceOut = checkItsNiceOut(currently);
   return [
     ...listAlerts,
-    ...addDivider(listAlerts, precipitation, niceOut),
-    ...precipitation,
-    ...addDivider(niceOut, precipitation, listAlerts),
     ...niceOut,
   ];
 }
 
-function addDivider(arr1: Message, arr2: Message, arr3: Message) {
-  return [
-    ...(arr1.length && (arr2.length || arr3.length)
-      ? [
-          {
-            type: "divider",
-          },
-        ]
-      : []),
-  ];
-}
 
 export type Message = [] | IncomingWebhookSendArguments["blocks"];
