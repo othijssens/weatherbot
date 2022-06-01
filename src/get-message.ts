c0dab8bd9e73886eaa419ad0c924079d79d76045
othijssens <olivier.thijssens@pacmed.nl>
2022-06-01 10:55:28 +0200

    remove precipitation

--- src/get-message.ts
+++ src/get-message.ts
@@ -1,12 +1,10 @@
 import { Weather } from "./get-weather";
-import getPrecipitation from "./precipitation";
 import checkItsNiceOut from "./nice-out";
 import getAlerts from "./alerts";
 import { IncomingWebhookSendArguments } from "@slack/webhook";
 
 export default function getMessage({ hourly, currently, alerts }: Weather) {
   const listAlerts = getAlerts(alerts);
-  const precipitation = getPrecipitation(hourly);
   const niceOut = checkItsNiceOut(currently);
   return [
     ...listAlerts,
